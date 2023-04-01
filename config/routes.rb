68bc83d9bb727363b5e27ad125d7689e76730d52
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-01 11:02:30 +0000

    add_navver

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  root to: 'homes/top'
+  get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
